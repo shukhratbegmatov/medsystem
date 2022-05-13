20d02c34f349635a14d7740b6daf33f92d430b14
shuukhratbegmatov <shuukhratbegmatov@gmail.com>
2022-05-13 14:34:36 +0500

    fix med front

---
+++ precache-manifest.e0e5d3ad8503473cd8c3f031dcc9b143.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6cf1bd32899223c2e56",
+    "url": "/css/app.091bd5f5.css"
+  },
+  {
+    "revision": "0cef6c02b7a3795656a9",
+    "url": "/css/chunk-vendors.baf9b5b7.css"
+  },
+  {
+    "revision": "a115fc6209a11c4e1d4344557ae25433",
+    "url": "/fonts/Manrope-Light.a115fc62.ttf"
+  },
+  {
+    "revision": "02a5dadf6053a510eadeff8eee8b5405",
+    "url": "/img/doc.02a5dadf.png"
+  },
+  {
+    "revision": "a29b86e0f13f93eae2452b3d2db65597",
+    "url": "/img/doc1.a29b86e0.png"
+  },
+  {
+    "revision": "0e5848728f644691053348f34384a898",
+    "url": "/img/image3.0e584872.png"
+  },
+  {
+    "revision": "fe1bac0bb07a091f2024608b94362f0c",
+    "url": "/img/image4.fe1bac0b.png"
+  },
+  {
+    "revision": "2c572bd4dc4f3f67a4b3c148aa4ecd5e",
+    "url": "/img/sitting6.2c572bd4.png"
+  },
+  {
+    "revision": "a08fe99aa2591526a0945922a6d574e2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7c699ab14885b7a9212",
+    "url": "/js/about.2cae30c8.js"
+  },
+  {
+    "revision": "e6cf1bd32899223c2e56",
+    "url": "/js/app.3ded793b.js"
+  },
+  {
+    "revision": "0cef6c02b7a3795656a9",
+    "url": "/js/chunk-vendors.e6361a1b.js"
+  },
+  {
+    "revision": "67cfd6992abed946ba10222442a51ea1",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
